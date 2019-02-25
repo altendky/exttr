103c6a44dfe4e86c60a147f2ed0d6037439fbcda
Kyle Altendorf <kyle.altendorf@epcpower.com>
2019-02-24 22:47:47 -0500

    Add @functools.wraps() to .ib() and .register_keywords()

--- src/exttr/__init__.py
+++ src/exttr/__init__.py
@@ -2,6 +2,8 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
+import functools
+
 from exttr.core import (
     AttrsCollisionError,
     get,
@@ -15,9 +17,11 @@ from exttr.core import (
 
 registry = Registry()
 
+@functools.wraps(registry.create_attribute)
 def ib(*args, **kwargs):
     return registry.create_attribute(*args, **kwargs)
 
+@functools.wraps(registry.register_keywords)
 def register_keywords(*args, **kwargs):
     return registry.register_keywords(*args, **kwargs)
 
