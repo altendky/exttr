ec858c46a63a3637c00019495591bf1ab359dab6
Kyle Altendorf <sda@fstab.net>
2019-02-28 14:07:08 -0500

    Add operating system classifiers

--- setup.py
+++ setup.py
@@ -29,6 +29,9 @@ setuptools.setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
     ],
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
     install_requires=[
